a1e18f58b4a27ccc8c207088a304a6251babf028
Andra Helton <andrahelton@gmail.com>
2023-03-29 19:22:08 -0600

    Add controllers

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -1,5 +1,11 @@
 class ResidentsController < ApplicationController
   def index
     @residents = Resident.all
+    @average_age = Resident.avg_age
+  end
+
+  def show
+    @resident = Resident.find(params[:id])
+    @courses = @resident.courses
   end
 end
